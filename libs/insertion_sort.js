0a70ff612aaf144057bd22b495506833088ab0da
ios122-air <yanfeng2016@vip.qq.com>
2016-04-28 14:23:04 +0800

    使用严格模式: 'use strict';

--- libs/insertion_sort.js
+++ libs/insertion_sort.js
@@ -1,3 +1,5 @@
+'use strict';
+
 /**
  * 插入排序.
  *
@@ -7,9 +9,9 @@
 function insertion_sort(A)
 {
   for (var j = 1; j < A.length; j++) {
-    key = A[j];
+    var key = A[j];
     // 插入 A[j] 到 已经排序的序列 A[1...j-1].
-    i = j - 1;
+    var i = j - 1;
     while (i >= 0 && A[i] > key) {
       A[i + 1] = A[i];
       i = i - 1;
